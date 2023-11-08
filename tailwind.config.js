43d14a6cd33ed560ebf9d3a042858051a7d63d19
akobir9990 <akobir.qoshshayev@gmail.com>
2023-11-08 19:06:13 +0500

    project have created, installed tailwind css, added JIT mode

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
